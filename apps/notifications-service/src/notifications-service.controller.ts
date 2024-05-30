358ef74f6fe18211ba2845b27dbb507763243783
Nathan Mande <neodarksoulink@gmail.com>
2024-05-30 06:38:25 +0200

    feat: message acknowledgement

--- apps/notifications-service/src/notifications-service.controller.ts
+++ apps/notifications-service/src/notifications-service.controller.ts
@@ -1,17 +1,30 @@
 import { Controller, Logger } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class NotificationsServiceController {
   private readonly logger = new Logger(NotificationsServiceController.name);
 
-  @EventPattern('notification.send') // 👈
-  sendNotification(@Payload() data: unknown) {
+  @EventPattern('notification.send')
+  sendNotification(@Payload() data: unknown, @Ctx() context: RmqContext) {
     // In the real-world application, this service would be responsible for notifying other services (or user) about the alarm.
     // For example, it could send an email to the building manager/department about the alarm.
     // It could also send an SMS message to the maintenance team.
     this.logger.debug(
       `Sending notification about the alarm: ${JSON.stringify(data)}`,
     );
+
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    if (originalMsg.fields.redelivered) {
+      this.logger.verbose(
+        `Message was already redelivered. Acknowledging the message and discarding it.`,
+      );
+      return channel.ack(originalMsg);
+    }
+
+    // Otherwise,reject the message and requeue it - just for the sake of this demo.
+    channel.nack(originalMsg);
   }
 }
